399f44b337754ff758f3cbf8153da19cc19cc332
Marc Seiler <mseiler@gmail.com>
2016-11-16 23:44:59 -0500

    fixed issue with screeps in variable names

--- scripts/run.sh
+++ scripts/run.sh
@@ -13,10 +13,8 @@ if [[ "${SCREEPS_STEAM_API_KEY}" == "" ]]; then
     echo "You must specify a SCREEPS_STEAM_API_KEY to run screeps!"
     exit 1
 fi
-setEnvironmentVariable 'SCREEPS_STEAM_API_KEY' $SCREEPS_STEAM_API_KEY
 
-
-for _curVar in `env | grep SCREEPS_ | awk -F = '{print $1}'`;do
+for _curVar in `env | grep ^SCREEPS_ | awk -F = '{print $1}'`;do
     key=${_curVar}
     val=${!_curVar}
     setEnvironmentVariable $key $val
